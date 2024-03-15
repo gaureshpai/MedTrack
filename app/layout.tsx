d4b6b779887c27859b5b05fff878d9d2c963e891
HimanshuHegde <himanshuhegde797@gmail.com>
2024-03-15 14:07:42 +0530

    style changes done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Navbar from "./components/Navbar";
+import "@/public/styles/globals.css"
 
 export const metadata: Metadata = {
     title: "Mini Project",
